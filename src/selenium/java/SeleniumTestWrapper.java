3e18b5ba66b3e2996fcd0fbc1e074bfc527400f0
Orri Axels <orriaxels@gmail.com>
2016-10-31 22:22:32 +0000

    Selenium test for player X

---
+++ src/selenium/java/SeleniumTestWrapper.java
@@ -0,0 +1,31 @@
+package ttt;
+
+import java.util.concurrent.TimeUnit;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+
+public abstract class SeleniumTestWrapper {
+    static ChromeDriver driver;
+    static String baseUrl;
+    static String port;
+
+    @BeforeClass
+    public static void openBrowser(){
+        driver = new ChromeDriver();
+        driver.manage().timeouts().implicitlyWait(10, TimeUnit.SECONDS);
+
+        port = System.getenv("PORT");
+        if (port == null) {
+            port = "4567";
+        }
+        baseUrl = "http://localhost:" + port;
+    }
+
+    @AfterClass
+    public static void closeBrowser(){
+        driver.quit();
+    }
+}
+
