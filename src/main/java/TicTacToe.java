b30090f01d95130c3fb6a53fedf8c50df81f6d86
Orri Arnarsson <orriarnars@gmail.com>
2016-10-31 08:11:59 +0000

    Initial commit
    Checking travis connection and gradle functionality between machines.

---
+++ src/main/java/TicTacToe.java
@@ -0,0 +1,21 @@
+package ttt;
+
+import java.util.Random;
+
+
+public class TicTacToe {
+
+    TicTacToe() {
+        
+    }
+
+    public String random() {
+        Random r = new Random();
+        return "#" + r.nextInt(10);
+    }
+
+
+    public static void main(String[] args) {
+        TicTacToe ttt = new TicTacToe();
+    }
+}
---
+++ src/main/java/tttWeb.java
@@ -0,0 +1,28 @@
+package ttt;
+
+import spark.*;
+import static spark.Spark.*;
+import spark.servlet.SparkApplication;
+
+
+public class tttWeb implements SparkApplication {
+
+    public static void main(String[] args) {
+        staticFileLocation("/public");
+        SparkApplication tttW = new tttWeb();
+
+        String port = System.getenv("PORT");
+        if (port != null) {
+            port(Integer.valueOf(port));
+        }
+
+        tttW.init();
+    }
+
+    @Override
+    public void init() {
+        final TicTacToe ttt = new TicTacToe();    
+        post("/random", (req, res) -> ttt.random());
+    }
+
+}
